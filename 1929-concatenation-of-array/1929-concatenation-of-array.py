512bcc6ebabcdcf3894dbd3581cd1c9fe23ca08b
SafiUllah Saleem <164955512+Safiullah-Saleem@users.noreply.github.com>
2025-05-12 19:41:27 +0500

    Time: 0 ms (100%), Space: 17.9 MB (44.3%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -11,4 +11,23 @@ class Solution:
         # return nums * 2
 
         # third solution 
-       return nums + nums
+    #    return nums + nums
+        result = []
+        for num in nums:
+            result.append(num)
+    # After this loop: result = [1, 2, 1]
+        for num in nums:
+            result.append(num)
+    # After this loop: result = [1, 2, 1, 1, 2, 1]
+        return result
+
+
+
+
+
+        
+        
+        # first output is [1,2,1]
+    
+            # second output is [1,2,1,1,2,1]
+        
